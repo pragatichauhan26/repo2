eb315747766ef8a95b4e5e4b89d623aa1bda5dfa
pragatichauhan26 <pragatichauhan890@gmail.com>
2021-09-16 19:14:44 +0530

    hi its 1st commit

---
+++ java2/src/java2/pragati.java
@@ -0,0 +1,10 @@
+package java2;
+
+public class pragati {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hi this is 1st prog");
+	}
+
+}
